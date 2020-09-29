0a1c5954d84d55502c65bf644c82e77e417cd38e
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-09-29 09:22:25 +0200

    multiply

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,22 @@
 package com.appslab;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(Arrays.toString(multiplyByLength(new int[]{2,3,1,0})));
+        System.out.println(Arrays.toString(multiplyByLength(new int[]{4,1,1})));
+        System.out.println(Arrays.toString(multiplyByLength(new int[]{1,0,3,3,7,2,1})));
+        System.out.println(Arrays.toString(multiplyByLength(new int[]{0})));
+    }
+    public static int[] multiplyByLength(int[] array){
+        int length = array.length;
+        for (int a = 0; a < length; a++ )
+        {
+            array[a] *= length;
+        }
+        return array;
     }
 }

7bb3760af70787f144153d98f6b93121a0e5d690
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-09-29 10:03:13 +0200

    palindrome

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,22 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(checkPalindrome("mom"));
+        System.out.println(checkPalindrome("scary"));
+        System.out.println(checkPalindrome("reviver"));
+        System.out.println(checkPalindrome("stressed"));
+    }
+    public static boolean checkPalindrome(String word){
+        char[] arrChar = word.toCharArray();
+        for (int i = 0; i < arrChar.length; i++ )
+        {
+            char a = arrChar[i];
+            char b = arrChar[arrChar.length - 1 - i];
+
+            if(a != b) {
+                return false;
+            }
+        }
+        return true;
     }
 }

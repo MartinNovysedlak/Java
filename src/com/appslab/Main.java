f797475dea827dde7adb7a0a318720420b21bc7e
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-09-29 09:38:29 +0200

    equalSlices

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,7 +3,14 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(equalSlices(11, 5,2));
+        System.out.println(equalSlices(11, 5,3));
+        System.out.println(equalSlices(8, 3,2));
+        System.out.println(equalSlices(8, 3,3));
+        System.out.println(equalSlices(24, 12,2));
     }
-    public static boolean equalSlices(int totalSlices, int noRecipients, int )
+    public static boolean equalSlices(int totalSlices, int noRecipients, int slicesEach) {
+        return noRecipients * slicesEach <= totalSlices;
+    }
 }
+

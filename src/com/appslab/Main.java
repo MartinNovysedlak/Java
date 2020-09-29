9abcd3260ac6fde2e95817de46bfea81d05565ca
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-09-29 09:04:19 +0200

    triangel

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,14 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(isTriangle(2,3,4));
+        System.out.println(isTriangle(3,4,5));
+        System.out.println(isTriangle(4,3,8));
+    }
+    public static boolean isTriangle(int a, int b, int c){
+     if(a + b > c && a + c > b && b + c > a) {
+         return true;
+     }
+     return false;
     }
 }

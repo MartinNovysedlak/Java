408c3a6490581f21d7c293ffb9cd442fb36b2e34
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-09-29 08:53:03 +0200

    equality

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,24 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        System.out.println(equality(3,4,3));
+        System.out.println(equality(1,1,1));
+        System.out.println(equality(3,4,1));
+    }
+    public static int equality (int a, int b, int c)
+    {
+        if (a == b || a == c)
+        {
+            if (b == c)
+            {
+                return 3;
+            }
+                return 2;
+        }
+        if(b==c)
+        {
+            return 2;
+        }
+        return 0;
     }
 }

bafc414ab58cfd79993fd00de44910e0740edaf2
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-10-01 09:26:28 +0200

    class 2

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,9 +3,7 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.setName("John");
-        student1.setStudentNumber(12345);
+        Student student1 = new Student(12345, "John");
         System.out.println(student1.getName() + ": " + student1.getStudentNumber());
     }
 }
--- src/com/appslab/Student.java
+++ src/com/appslab/Student.java
@@ -3,6 +3,13 @@ package com.appslab;
 public class Student {
     private int studentNumber = 12345;
     private String name = "John";
+    public void setName(String Name, int studentNumber){
+    }
+
+    public Student(int studentNumber, String name) {
+        this.studentNumber = studentNumber;
+        this.name = name;
+    }
 
     public int getStudentNumber() {
         return studentNumber;

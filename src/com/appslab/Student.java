bd8801bb06d6ca840a8e75ff53c52397483eb915
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-10-01 09:19:37 +0200

    class

---
+++ src/com/appslab/Student.java
@@ -0,0 +1,22 @@
+package com.appslab;
+
+public class Student {
+    private int studentNumber = 12345;
+    private String name = "John";
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

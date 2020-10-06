e8ac5b352843d178ea142f945846bb8c698db4b4
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-10-06 08:51:19 +0200

    12b

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,7 +3,9 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        Student student1 = new Student(12345, "John");
-        System.out.println(student1.getName() + ": " + student1.getStudentNumber());
+        Student student1 = new Student(2, "John", "123456", "01001 Zilina");
+        Student student2 = new Student(5, "Sam", "456789", "01001 Zilina" );
+        System.out.println(student1.getName() + ": " + student1.getStudentNumber()  + "," + student1.getPhoneNumber() + "," + student1.getAddress());
+        System.out.println(student2.getName() + ": " + student2.getStudentNumber() + "," + student2.getPhoneNumber() + "," + student2.getAddress());
     }
 }
--- src/com/appslab/Student.java
+++ src/com/appslab/Student.java
@@ -3,12 +3,16 @@ package com.appslab;
 public class Student {
     private int studentNumber = 12345;
     private String name = "John";
-    public void setName(String Name, int studentNumber){
+    private String phoneNumber = "0948000555";
+    private String address = "Čadca 144";
+    public void setName(String Name, int studentNumber, String phoneNumber, String address){
     }
 
-    public Student(int studentNumber, String name) {
+    public Student(int studentNumber, String name, String phoneNumber, String address) {
         this.studentNumber = studentNumber;
         this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
     }
 
     public int getStudentNumber() {
@@ -25,5 +29,17 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress(){ return address;}
+
+    public String getPhoneNumber(){ return phoneNumber;}
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }

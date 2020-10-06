8a9c81854cf85a3b78191d5b95bfd0b9fe866b49
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-10-06 09:47:30 +0200

    Sum of People

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,24 @@
 package com.appslab;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        Person person1 = new Person("John", 21,23000);
+        Person person2 = new Person("Steve", 32,40000);
+        Person person3 = new Person("Martin", 16,2700);
+        System.out.println("Name:" + person1.getName() + ",Age:" + person1.getAge() + ",Budget:" + person1.getBudget());
+        System.out.println("Name:" + person2.getName() + ",Age:" + person2.getAge() + ",Budget:" + person2.getBudget());
+        System.out.println("Name:" + person3.getName() + ",Age:" + person3.getAge() + ",Budget:" + person3.getBudget());
+        System.out.println(sum(new Person[] {person1, person2, person3}));
+    }
+    public static int sum(Person[] sume){
+        var budget = 0;
+        for(Person person:sume){
+            budget += person.getBudget();
+        }
+        return budget;
     }
 }
